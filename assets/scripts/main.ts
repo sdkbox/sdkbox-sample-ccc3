ba0597e895ab77bff3a57330a039910f2d53ac87
hugo <hugohuang1111@gmail.com>
2021-11-12 16:40:58 +0800

    add logarea for iap

--- assets/scripts/main.ts
+++ assets/scripts/main.ts
@@ -1,5 +1,5 @@
 
-import { _decorator, Component, Node } from 'cc';
+import { _decorator, Component, Node, Label } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('Main')
@@ -11,9 +11,12 @@ export class Main extends Component {
     // @property
     // serializableDummy = 0;
 
+    @property(Label)
+    logArea: Label | null = null;
+
     start () {
         // [3]
-        console.log(`SDKBox TS start`);
+        this.log(`SDKBox TS start`);
     }
 
     // update (deltaTime: number) {
@@ -28,12 +31,12 @@ export class Main extends Component {
 
     onButtonStart() {
         if (!this.checkSDKBox()) { return; }
-        console.log(`SDKBox TS onButtonStart`);
+        this.log(`SDKBox TS onButtonStart`);
     }
 
     checkSDKBox(): boolean {
         if ('undefined' == typeof sdkbox) {
-            console.log("Can't detect sdkbox");
+            this.log("Can't detect sdkbox");
             return false;
         }
 
@@ -41,13 +44,95 @@ export class Main extends Component {
     }
 
     initIAP() {
-        console.log(`to init IAP`);
-        try {
-            sdkbox.IAP.init();
-        } catch (error) {
-            console.error(error.toString());
-            console.log(error.stack);
+        this.log(`to init IAP`);
+
+        const self = this;
+        sdkbox.IAP.setListener({
+            onSuccess : function (product) {
+                //Purchase success
+                self.log("Purchase successful: " + product.name);
+                self.printProduct(product);
+            },
+            onFailure : function (product, msg) {
+                //Purchase failed
+                //msg is the error message
+                self.log("Purchase failed: " + product.name + " error: " + msg);
+
+            },
+            onCanceled : function (product) {
+                //Purchase was canceled by user
+                self.log("Purchase canceled: " + product.name);
+            },
+            onRestored : function (product) {
+                //Purchase restored
+                self.log("Restored: " + product.name);
+
+                self.printProduct(product);
+            },
+            onProductRequestSuccess : function (products) {
+                //Returns you the data for all the iap products
+                //You can get each item using following method
+                for (var i = 0; i < products.length; i++) {
+                    self.printProduct(products[i]);
+                    // self.log("================");
+                    // self.log("name: " + products[i].name);
+                    // self.log("price: " + products[i].price);
+                    // self.log("priceValue: " + products[i].priceValue);
+                    // self.log("================");
+                }
+            },
+            onProductRequestFailure : function (msg) {
+                //When product refresh request fails.
+                self.log("Failed to get products");
+            },
+            onShouldAddStorePayment: function(productId) {
+                self.log("onShouldAddStorePayment:" + productId);
+                return true;
+            },
+            onFetchStorePromotionOrder : function (productIds, error) {
+                self.log("onFetchStorePromotionOrder:" + " " + " e:" + error);
+            },
+            onFetchStorePromotionVisibility : function (productId, visibility, error) {
+                self.log("onFetchStorePromotionVisibility:" + productId + " v:" + visibility + " e:" + error);
+            },
+            onUpdateStorePromotionOrder : function (error) {
+                self.log("onUpdateStorePromotionOrder:" + error);
+            },
+            onUpdateStorePromotionVisibility : function (error) {
+                self.log("onUpdateStorePromotionVisibility:" + error);
+            },
+        });
+
+        sdkbox.IAP.init();
+
+    }
+
+    printProduct(p: any) {
+        // this.log("======The product info======");
+
+        this.log(p.name + ":" + p.price);
+
+        // this.log("name=" + p.name);
+        // this.log("title=" + p.title);
+        // this.log("description=" + p.description);
+        // this.log("price=" + p.price);
+        // this.log("priceValue=" + p.priceValue);
+        // this.log("currencyCode=" + p.currencyCode);
+        // this.log("receipt=" + p.receipt);
+        // this.log("receiptCipheredPayload=" + p.receiptCipheredPayload);
+        // this.log("transactionID=" + p.transactionID);
+        // this.log("");
+    }
+
+    log(s: string) {
+        console.log(s);
+        if (null == this.logArea) { return; }
+        let lines = this.logArea.string.split('\n');
+        while (lines.length > 4) {
+            lines.shift();
         }
+        lines.push(s);
+        this.logArea.string = lines.join('\n');
     }
 
 }
